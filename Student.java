30214be2d2cf87c8938960d087849887e0b5383d
SinaliW <sinalirovinya2002@gmail.com>
2025-09-07 17:28:57 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,31 @@
+public class Student {
+    private int id;
+    private String name;
+    private int age;
+    private String grade;
+
+    // Constructor
+    public Student(int id, String name, int age, String grade) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
+    }
+
+    public Student(String name, int age, String grade) {
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
+    }
+
+    // Getters & Setters
+    public int getId() { return id; }
+    public String getName() { return name; }
+    public int getAge() { return age; }
+    public String getGrade() { return grade; }
+
+    @Override
+    public String toString() {
+        return id + " | " + name + " | " + age + " | " + grade;
+    }
+}
